066c0d4a141d5479b81e543b01bb3e2dd94e3709
charliechen200iq <108031390+charliechen200iq@users.noreply.github.com>
2023-05-15 20:05:55 +1200

    coordinate Game_of_Life.java

--- Game_of_Life.java
+++ Game_of_Life.java
@@ -7,8 +7,13 @@
 import java.util.Scanner;
 public class Game_of_Life
 {
+    Scanner keyboard = new Scanner(System.in);
+    
     int gridSize = 30;
     String [][] grid = new String [gridSize][gridSize];
+    
+    int coordinateX = 0;
+    int coordinateY = 0;
     
     /**
      * Constructor for objects of class Game_of_Life
@@ -39,15 +44,97 @@ public class Game_of_Life
     
     void instruction()
     {
-        System.out.println("T - turning cells on:' o ' or off:' - '");
-        System.out.println("A - advancing a generation");
-        System.out.println("M - advancing a provided number of generations");
-        System.out.println("R - resetting all the cells to off");
-        System.out.println("Q - quitting");
+        System.out.println("t - turning cells on:' o ' or off:' - '");
+        System.out.println("a - advancing a generation");
+        System.out.println("m - advancing a provided number of generations");
+        System.out.println("r - resetting all the cells to off");
+        System.out.println("q - quitting");
     }
     
     void menu()
     {
+        String input = keyboard.nextLine().toLowerCase();
+        switch (input){
+            case "t": onOff();
+            break;
+            case "a": oneGeneration();
+            break;
+            case "m": multipleGenertaion();
+            break;
+            case "r": reset();
+            break;
+            case "q": quit();
+            break;
+            default: 
+            System.out.println("");
+            System.out.println("Wrong input:(");
+            menu();
+            break;
+        }
+    }
+    
+    void onOff()
+    {
+        System.out.println("t - turning cells on:' o ' or off:' - '");
+        getCoordinates("turn the cells on or off by enter its coordinate in the form - x,y");
+        newGrid();
+        displayGrid();
+    }
+    
+    void getCoordinates(String prompt)
+    {
+        System.out.println(prompt);
+        String[] numbers = keyboard.nextLine().split(",");
+        while (numbers.length !=2 || checkNumbers(numbers) != true){
+            System.out.println("incorrect input :( " + prompt);
+            numbers = keyboard.nextLine().split(",");
+        }
+        coordinateX = Integer.parseInt(numbers[0]);
+        coordinateY = Integer.parseInt(numbers[1]);
+    }
 
+    boolean checkNumbers(String[] integers)
+    {
+        for (int i=0; i<integers.length;i++){
+            for (int j=0; j<integers[i].length();j++){
+                if(integers[i].charAt(j) > '9' || integers[i].charAt(j) < '0'){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    void newGrid(){
+        if (grid[coordinateX][coordinateY] == " - "){
+            grid[coordinateX][coordinateY] = " O ";
+        } else{
+            grid[coordinateX][coordinateY] = " - ";
+        }
+    }
+    
+    
+    
+    
+    
+    void oneGeneration()
+    {
+        System.out.println("a - advancing a generation");
+    }
+    
+    void multipleGenertaion()
+    {
+        System.out.println("m - advancing a provided number of generations");
+    }
+    
+    void reset()
+    {
+        System.out.println("r - resetting all the cells to off");
+    }
+    
+    void quit()
+    {
+        System.out.println("q - quitting");
     }
 }
+
